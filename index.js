e5e8187dec653d3d19df0bc391ee11fd55c8ec34
charlesmaillu <charles972@live.com>
2020-07-21 07:46:22 +0300

    make changes in index.js file

--- index.js
+++ index.js
@@ -47,66 +47,11 @@ function checkGender(){
   var gen = document.getElementsByName("rads");
   if(gen[0].checked == true){
       var gender = "male";
+      document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[day]} and your Akan name is ${maleNames[day]}`;
   }else if(gen[1].checked == true){
       var gender = "female";
-  }else {
-    console.log("pass");//Test the radio buttons
+      document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[day]} and your Akan name is ${femaleNames[day]}`;
+  }else{
+      document.getElementById("result").innerHTML= `select gender`;
   }
-    switch(gender){
-        case gender = "male":
-              switch(day){
-                case (0 || -0):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[0]} and your Akan name is ${maleNames[0]}`;
-                break;
-                case (1 || -1):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[1]} and your Akan name is ${maleNames[1]}`;
-                break;
-                case (2 || -2):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[2]} and your Akan name is ${maleNames[3]}`;
-                break;
-                case (3 || -3):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[3]} and your Akan name is ${maleNames[3]}`;
-                break;
-                case (4 || -4):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[4]} and your Akan name is ${maleNames[4]}`;
-                break;
-                case (5 || -5):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[5]} and your Akan name is ${maleNames[5]}`;
-                break;
-                case (6 || -6):
-                  document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[6]} and your Akan name is ${maleNames[6]}`;
-                break;
-                default:
-                // console.console.log("Pass");//Test male case
-              }
-        break;
-        case gender = "female":
-                switch(day){
-                  case (0 || -0):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[0]} and your Akan name is ${femaleNames[0]}`;
-                  break;
-                  case (1 || -1):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[1]} and your Akan name is ${femaleNames[1]}`;
-                  break;
-                  case (2 || -2):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[2]} and your Akan name is ${femaleNames[2]}`;
-                  break;
-                  case (3 || -3):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[3]} and your Akan name is ${femaleNames[3]}`;
-                  break;
-                  case (4 || -4):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[4]} and your Akan name is ${femaleNames[4]}`;
-                  break;
-                  case (5 || -5):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[5]} and your Akan name is ${femaleNames[5]}`;
-                  break;
-                  case (6 || -6):
-                    document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[6]} and your Akan name is ${femaleNames[6]}`;
-                  break;
-
-              }
-        break
-        default:
-        console.log("pass");//Test gender switch
-    }
 }
