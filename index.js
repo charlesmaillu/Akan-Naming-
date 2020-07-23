5edf68ae775934d926d605e0f84c4a3180596943
charlesmaillu <charles972@live.com>
2020-07-23 12:13:14 +0300

    add alert messages

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ function getInput(){
   dayOfMonth = parseInt(document.getElementById("monthday").value);
   form = document.getElementById('form')
 }
-
-
 //Calculate func
 function calculateDay(){
     getInput();
@@ -16,32 +14,37 @@ function calculateDay(){
     console.log(dayOfWeek); //Test the calculateDay function
     return (Math.floor(dayOfWeek));
 }
-
 //main caller func
  function checkDayOfWeek(){
      day = calculateDay();
       checkGender();
       console.log("The function runs");//Test chackDayOfWeek function
 }
-
 //arrays
 let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames =[" Akosua"," Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-
-
 //get selected radio button
 function checkGender(){
   var gen = document.getElementsByName("rads");
   if(gen[0].checked == true && century !== "" &&  dayOfMonth !== "" &&  month !== "" && year !== "" && dayOfMonth > 0 && dayOfMonth <31 && month > 0 && month < 12 && year > 0 && year < 100){
-      var gender = "male";
-      document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[day]} and your Akan name is ${maleNames[day]}`;
+        document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[day]} and your Akan name is ${maleNames[day]}`;
   }else if(gen[1].checked == true && century !== "" &&  dayOfMonth !== "" &&  month !== "" && year !== "" && dayOfMonth > 0 && dayOfMonth <31 && month > 0 && month < 12 && year > 0 && year < 100){
-      var gender = "female";
-      document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[day]} and your Akan name is ${femaleNames[day]}`;
+        document.getElementById("result").innerHTML = `You were born on a ${daysOfWeek[day]} and your Akan name is ${femaleNames[day]}`;
+  }else if(gen[1].checked == true || gen[0].checked == true && century === "" &&  dayOfMonth !== "" &&  month !== "" && year !== "" && dayOfMonth > 0 && dayOfMonth <31 && month > 0 && month < 12 && year > 0 && year < 100){
+        document.getElementById("result").innerHTML = `enter century data`;
+        alert('enter century data')
+  }else if(gen[1].checked == true || gen[0].checked == true && century !== "" &&  dayOfMonth == "" &&  month !== "" && year !== "" || dayOfMonth < 0 || dayOfMonth >31 && month > 0 && month < 12 && year > 0 && year < 100){
+        document.getElementById("result").innerHTML = ` date`;
+        alert('enter correct date')
+  }else if(gen[1].checked == true || gen[0].checked == true && century !== "" &&  dayOfMonth !== "" &&  month !== "" && year == "" && dayOfMonth > 0 || dayOfMonth <31 && month > 0 && month < 12 || year < 0 || year > 100){
+        document.getElementById("result").innerHTML = ` date`;
+        alert('enter correct year')
   }else if(gen[0].checked == false || gen[1].checked == false){
-	document.getElementById("result").innerHTML ='select gender'
+        document.getElementById("result").innerHTML ='select gender'
+        alert('select gender')
   }else{
       document.getElementById("result").innerHTML= `insert correct data`;
+      alert('fill all fields')
   }
 }
